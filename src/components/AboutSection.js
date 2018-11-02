0fb1aa8343fee5cdbeff5423a7ee71e0a2946778
Nick James <thenickjames@gmail.com>
2018-11-02 16:03:35 -0700

    working on about me section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
-import { Grid, Hidden, withStyles } from "@material-ui/core";
+import { Grid, Typography, withStyles } from "@material-ui/core";
 import withWidth, { isWidthDown } from "@material-ui/core/withWidth";
 import { Element } from "react-scroll";
+import classNames from 'classnames';
 import Background from "../assets/images/house1.jpg";
 import { BackgroundContainer } from "./Background";
 import PageContent from "./PageContent";
 
-const styles = theme => ({});
+const Header = withStyles({
+  root: {}
+})(props => {
+  const { classes, className: classNameProp } = props;
+  const className = classNames(classes.root, classNameProp);
+
+  return (
+    <Grid
+      item
+      container
+      className={className}
+      direction="column"
+      alignItems="center"
+    >
+      <Grid item>
+        <Typography className={classes.heading} variant="h3">
+          Why Choose Me
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+});
+
+const styles = theme => ({
+  content: {
+    paddingTop: "40px",
+    paddingBottom: "40px",
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "80px",
+      paddingBottom: "80px"
+    }
+  },
+});
 
 class AboutSection extends Component {
   constructor(props) {
@@ -19,8 +52,8 @@ class AboutSection extends Component {
     return (
       <Element name="about">
         <BackgroundContainer justify="center" src={Background}>
-          <PageContent justify="center" alignItems="center">
-            <h1>About</h1>
+          <PageContent className={classes.content} justify="center" alignItems="center">
+            <Header />
           </PageContent>
         </BackgroundContainer>
       </Element>
