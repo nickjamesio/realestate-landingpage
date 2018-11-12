d0f01fd431bd028bfad1c2b5ebf89aaae26b6fda
Nick James <thenickjames@gmail.com>
2018-11-11 23:12:00 -0800

    flask validation, flask docker, web optomized pics

---
+++ flask/src/app.py
@@ -0,0 +1,85 @@
+import json
+import re
+import logging
+from flask import Flask
+from flask import request
+from mailchimp3 import MailChimp
+from mailchimp3.mailchimpclient import MailChimpError
+from helpers import initialize_logging, get_missing_keys, get_empty_keys, check_email, check_phone, check_price, check_transaction
+
+api_key = "34054e1fbd8462fa76a3a42b83b9283f-us19"
+user = "kingjames3"
+list_id = "14bfc390f7"
+tags = {
+    "buy": "buy",
+    "sell": "sell",
+    "investor": "investor"
+}
+
+app = Flask(__name__)
+initialize_logging()
+
+
+@app.route("/clients", methods=["POST"])
+def clients():
+    response = {"message": ""}
+
+    if not request.is_json:
+        response["message"] = "Missing 'application/json' header"
+        response["status"] = 400
+
+    data = request.json
+    keys = ["name", "email", "phone"]
+    missing_keys = get_missing_keys(keys, data)
+    empty_keys = get_empty_keys(keys, data)
+
+    if missing_keys:
+        response["message"] = "The following keys must be set in payload [{}]".format(
+            ", ".join(missing_keys))
+        response["status"] = 400
+    elif empty_keys:
+        response["message"] = "The following keys cannot have an empty value [{}]".format(
+            ", ".join(empty_keys))
+        response["status"] = 400
+    elif not check_email(data["email"]):
+        # TODO replace with neverbounce request
+        response["message"] = "Invalid 'email' value: '{}'".format(
+            data["email"])
+        response["status"] = 400
+    elif not check_phone(data["phone"]):
+        response["message"] = "Invalid 'phone' value: '{}'".format(
+            data["phone"])
+        response["status"] = 400
+    elif "transaction" in data and not check_transaction(data["transaction"]):
+        response["message"] = "Invalid 'transaction' value: '{}'".format(
+            data["transaction"])
+        response["status"] = 400
+
+    # Assuming everything passed
+    client = MailChimp(mc_api=api_key, mc_user=user)
+    try:
+        request_data = {
+            "email_address": data["email"],
+            "status": "subscribed",
+            "merge_fields": {
+                "FNAME": data["name"],
+                "PHONE": data["phone"]
+            },
+            "ip_signup": request.remote_addr
+        }
+        request_data["tags"] = [data["transaction"]]
+        response = client.lists.members.create(
+            list_id=list_id, data=request_data)
+    except MailChimpError as e:
+        errors = e.args[0]
+        response["message"] = errors["detail"]
+        response["status"] = errors["status"]
+    except Exception as e:
+        response["message"] = str(e)
+        response["status"] = 500
+    else:
+        response["message"] = "Successfully added '{}' to mail list".format(
+            data["email"])
+        response["status"] = 200
+    finally:
+        return json.dumps(response)
---
+++ flask/src/helpers.py
@@ -0,0 +1,85 @@
+import re
+import logging
+
+def initialize_logging():
+    """
+    Initilize logging for app
+    """
+    fh = logging.FileHandler('/path/to/some/log.log')
+    logger = logging.getLogger('mailchimp3.client')
+    logger.addHandler(fh)
+
+def check_price(price):
+    pass
+
+def check_transaction(transaction):
+    """
+    Check for valid transaction value
+    :transaction: string
+    :return: bool
+    """
+    valid_values = ["buy", "sell"]
+    valid = False
+    for value in valid_values:
+        if value == transaction:
+            valid = True
+            break
+    return valid
+
+
+def check_phone(phone):
+    """
+    Function that verifies that the string passed is a valid phone number.
+    :param phone: The potential phone number
+    :type phone: string
+    :return: bool
+    """
+    if re.match(r"^[0-9-()+]+$", phone):
+        return True
+    return False
+
+
+def check_email(email):
+    """
+    Function that verifies that the string passed is a valid email address.
+    Regex for email validation based on MailChimp limits:
+    http://kb.mailchimp.com/accounts/management/international-characters-in-mailchimp
+    :param email: The potential email address
+    :type email: string
+    :return: bool
+    """
+    if re.match(r"^.+@.+\..+$", email):
+        return True
+    return False
+
+
+def get_empty_keys(keys, data):
+    """
+    Retrieve list of empty keys in json body
+    :param keys: list
+      List of keys we care about
+    :param data: dict
+      Dictionary of json payload
+    :return: list
+    """
+    empty = []
+    for key in keys:
+        if not data[key]:
+            empty.append(key)
+    return empty
+
+
+def get_missing_keys(expected, actual):
+    """
+    Retrieve list of missing keys in json body
+    :param expected: list
+      List of expected keys
+    :param actual: dict
+      Dictionary of json payload
+    :return: list
+    """
+    missing = []
+    for key in expected:
+        if key not in actual:
+            missing.append(key)
+    return missing
