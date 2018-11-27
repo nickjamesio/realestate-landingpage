d6f6ea956bed94faa9e356fcfb0f6ededfccccbb
Nick James <thenickjames@gmail.com>
2018-11-27 15:36:02 -0800

    react static is now fully working

--- react/src/components/HomeSection.js
+++ react/src/components/HomeSection.js
@@ -1,35 +1,35 @@
 import React, { Component } from "react";
 import { Grid, Hidden, withStyles } from "@material-ui/core";
-import { Element } from 'react-scroll';
+import { Element } from "react-scroll";
 import Background from "../assets/images/House.jpg";
 import Realtor from "../assets/images/MomStanding.png";
 import SubscribeCard from "./SubscribeCard";
-import BackgrounImage from './Background';
-import PageContent from './PageContent';
+import BackgrounImage from "./Background";
+import PageContent from "./PageContent";
 
 const styles = theme => ({
   content: {
     justifyContent: "center",
     [theme.breakpoints.up("md")]: {
-      justifyContent: "space-between",
+      justifyContent: "space-between"
     }
   },
   subscribeCard: {
-    paddingTop: '40px',
-    paddingBottom: '40px',
-    [theme.breakpoints.up('md')]: {
-      paddingTop: '80px',
-      paddingBottom: '80px'
+    paddingTop: "40px",
+    paddingBottom: "40px",
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "80px",
+      paddingBottom: "80px"
     }
   },
   realtorContainer: {
-    alignSelf: "flex-end",
     height: "600px",
+    alignSelf: "flex-end"
   },
   realtor: {
-    height: '100%',
+    height: "600px",
     width: "auto"
-  },
+  }
 });
 
 class HomeSection extends Component {
@@ -44,11 +44,15 @@ class HomeSection extends Component {
       <Element name="home">
         <BackgrounImage justify="center" src={Background}>
           <PageContent className={classes.content}>
-            <Hidden smDown>
-              <Grid item className={classes.realtorContainer}>
-                <img src={Realtor} className={classes.realtor} />
-              </Grid>
+            <Hidden
+              smDown
+              implementation="css"
+              initialWidth="md"
+              className={classes.realtorContainer}
+            >
+              <img src={Realtor} className={classes.realtor} />
             </Hidden>
+            
             <Grid item>
               <SubscribeCard className={classes.subscribeCard} />
             </Grid>
